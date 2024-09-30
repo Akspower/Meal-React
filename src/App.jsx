91580a5337d9c9fe3962070e2aac2cb09996231d
Akspower <adityakumarsharmapower@gmail.com>
2024-09-30 10:25:01 +0530

    Fixed Meal.jsx fetch logic and updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App(){
 
   return(
 <div>
-{/* <Movie/> */}
+<Movie/>
 
 {/* <UseEffect/> */}
 
@@ -20,7 +20,7 @@ function App(){
 
 {/* <MultipleInputHandle/> */}
 
-<Meal/>
+{/* <Meal/> */}
   </div>
   );
 
